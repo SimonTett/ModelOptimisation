e76cc3fa9a87b41aefe09c05570f9f218c529424
Simon Tett <stett2@node1h21.ecdf.ed.ac.uk>
2023-02-22 13:39:59 +0000

    Cleanup for eddie processing. Fixed getting a 0 file  and reduced output.

--- eddie/postProcess.sh
+++ eddie/postProcess.sh
@@ -3,37 +3,33 @@
 # takes 2 arguments
 # dirListFile -- file containing the directories to process
 # jsonFile -- the filename of the JSON configuration file.
-echo "Args are $*"
 for var in "$@" ; do
-    echo ">>>$var<<<"
+    echo "ARG: >>>$var<<<"
 done
 DirListFile=$1  # file containing list of directories.
 JsonFile=$2  # json file
-echo "DirListFile is $DirListFile"
-echo "JsonFile is $JsonFile"
 JsonFile=$(realpath $JsonFile)
-echo "going to post process using  $JsonFile in $PWD"
-echo pwd=$PWD
+echo "going to post process using  $JsonFile in $PWD from $DirListFile"
 . /etc/profile.d/modules.sh
-#. ./setupPy.sh ## run the setup script which sets up paths etc.
-echo "Array ID "$SGE_TASK_ID
-if [ $SGE_TASK_ID > 0 ] ; then
-    if [  ! -f $dirListFile ] ;  then
+echo "Array ID $SGE_TASK_ID"
+if [[ $SGE_TASK_ID -gt 0 ]] ; then
+    if [[  ! -f $DirListFile ]] ;  then
        pwd
-       echo in wrong place?
-       echo not found $dirListFile
+       echo "in wrong place?"
+       echo "not found $DirListFile"
        exit 1
-    else # extract directory and name of output file.
-       line=$(awk "NR==$SGE_TASK_ID" $DirListFile)
-       # now split line on , into dir, ppScript and PostProcessOutput
-       dir=$(echo $line | cut -f1 -d",")
-       PostProcessScript=$(echo $line | cut -f2 -d",")
-       PostProcessOutput=$(echo $line | cut -f3 -d",")
-       echo $line
-       cd $dir
-       echo "going to run $PostProcessScript $JsonFile $PostProcessOutput in $PWD"
-       $PostProcessScript $JsonFile $PostProcessOutput
-  fi
+    fi
+    # extract directory and name of output file.
+    line=$(awk "NR==$SGE_TASK_ID" $DirListFile)
+    # now split line on , into dir, ppScript and PostProcessOutput
+    dir=$(echo $line | cut -f1 -d",")
+    PostProcessScript=$(echo $line | cut -f2 -d",")
+    PostProcessOutput=$(echo $line | cut -f3 -d",")
+    cd $dir
+    echo "going to run $PostProcessScript $JsonFile $PostProcessOutput in $PWD"
+    $PostProcessScript $JsonFile $PostProcessOutput
+else
+    echo "SGE_TASK_ID <= 0 = $SGE_TASK_ID"
 fi
 
 
