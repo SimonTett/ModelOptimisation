015c6b834f36501a2c6ed76a7508ec832ad3b4bb
David de Klerk <david.deklerk@ed.ac.uk>
2025-03-31 09:31:48 +0100

    Add OptClim related tasks for UM Cylc/Rose suites.
     * Add OptClim specific tasks for the UM run in Cylc/Rose suites
     * Add prototype code and unit tests to merge these tasks with UM runs.

---
+++ OptClimVn3/configurations/example_UM_rose/references/optclim_tasks/app/optclim_pre/bin/optclim_pre.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+echo "Optclim Preprocessing"
+echo " ENV_VAR=${ENV_VAR}"
+
+echo "hostname:"
+hostname 
+
+echo "pwd:"
+pwd
+
+echo "ls:"
+ls
+
+echo "export:"
+export
+
---
+++ OptClimVn3/configurations/example_UM_rose/references/optclim_tasks/app/optclim_um_fail/bin/optclim_um_fail.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+echo "Optclim UM Failure"
+
+echo "hostname:"
+hostname 
+
+echo "pwd:"
+pwd
+
+echo "ls:"
+ls
+
+echo "export:"
+export
+
